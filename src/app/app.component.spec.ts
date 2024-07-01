789febe539dacd4904ad3493797f5ffe2cbe202b
Andreas Rümpel <andreas.ruempel@planyverse.com>
2024-07-01 12:42:32 +0200

    Fix test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,6 @@ describe('AppComponent', () => {
   it(`should have the 'pspdfkit-angular-oom-demo' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pspdfkit-angular-oom-demo');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, pspdfkit-angular-oom-demo');
+    expect(app.title).toEqual('PSPDFKit for Web Angular Example');
   });
 });
